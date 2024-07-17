9ae34f54117e60e91823d62a0ec53ec94ec1e60f
ezeun <consistency1231@gmail.com>
2024-07-17 10:35:32 +0900

    2) Repository 작성

---
+++ back/src/main/java/com/ssafy/back/repository/CertificationRepository.java
@@ -0,0 +1,11 @@
+package com.ssafy.back.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ssafy.back.entity.CertificationEntity;
+
+@Repository
+public interface CertificationRepository extends JpaRepository<CertificationEntity, String> {
+    
+}
---
+++ back/src/main/java/com/ssafy/back/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.ssafy.back.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ssafy.back.entity.UserEntity;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, String> {
+    
+}
