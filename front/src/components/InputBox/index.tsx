5b63e6264314ff33ab71a2346300456a6b73e973
ezeun <consistency1231@gmail.com>
2024-07-17 13:13:20 +0900

    5) response 공통 타입(code, message) 선언

---
+++ front/src/components/InputBox/index.tsx
@@ -0,0 +1,14 @@
+import { forwardRef } from "react";
+
+interface Props{
+
+}
+
+const InputBox = forwardRef<HTMLInputElement, Props>((props: Props, ref) => {
+
+    return (
+        <div>
+
+        </div>
+    );
+});
